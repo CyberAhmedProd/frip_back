7da239c9249306bdca4efdf4d0daa8f9857e2cfa
CyberAhmedProd <gharbiahmedpro@gmail.com>
2021-04-23 11:35:38 +0200

    add some attributs for order model

--- src/main/java/com/teamyostrik/efrip/models/Order.java
+++ src/main/java/com/teamyostrik/efrip/models/Order.java
@@ -4,34 +4,36 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Document (collection = "order")
 public class Order {
-
     @Id
     private String id;
-
     @DBRef
     private User user;
-
     @DBRef
     private List<Product> products;
-
-
     private Address billingAddress;
-
+    private OrderStatus status;
+    private Timestamp orderedDate;
+    private Timestamp shippedDate;
+    private float totalPrice;
     public Order() {
     }
-
-    public Order(String id, User user, List<Product> products, Address billingAddress) {
-        this.id = id;
-        this.user = user;
-        this.products = products;
-        this.billingAddress = billingAddress;
-    }
-
-    public String getId() {
+    public Order(User user, List<Product> products, Address billingAddress, OrderStatus status, Timestamp orderedDate,
+			Timestamp shippedDate, float totalPrice) {
+		super();
+		this.user = user;
+		this.products = products;
+		this.billingAddress = billingAddress;
+		this.status = status;
+		this.orderedDate = orderedDate;
+		this.shippedDate = shippedDate;
+		this.totalPrice = totalPrice;
+	}
+	public String getId() {
         return id;
     }
 
@@ -62,4 +64,28 @@ public class Order {
     public void setBillingAddress(Address billingAddress) {
         this.billingAddress = billingAddress;
     }
+	public OrderStatus getStatus() {
+		return status;
+	}
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	public Timestamp getOrderedDate() {
+		return orderedDate;
+	}
+	public void setOrderedDate(Timestamp orderedDate) {
+		this.orderedDate = orderedDate;
+	}
+	public Timestamp getShippedDate() {
+		return shippedDate;
+	}
+	public void setShippedDate(Timestamp shippedDate) {
+		this.shippedDate = shippedDate;
+	}
+	public float getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(float totalPrice) {
+		this.totalPrice = totalPrice;
+	}
 }

d1b460d4b00e0cde750c8c9f37f6c4de99fb0d24
CyberAhmedProd <gharbiahmedpro@gmail.com>
2021-04-28 01:03:38 +0200

    add photo controller

--- src/main/java/com/teamyostrik/efrip/controllers/PhotoController.java
+++ src/main/java/com/teamyostrik/efrip/controllers/PhotoController.java
@@ -1,11 +1,22 @@
 package com.teamyostrik.efrip.controllers;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.teamyostrik.efrip.services.PhotoService;
 public class PhotoController {
+	@Autowired
+	private PhotoService PhotoService;
 	@PostMapping("/photos/add")
 	public String addPhoto(@RequestParam("title") String title, 
 	  @RequestParam("image") MultipartFile image, Model model) 
 	  throws IOException {
-	    String id = photoService.addPhoto(title, image);
+	    String id = PhotoService.addPhoto(title, image);
 	    return "redirect:/photos/" + id;
 	}
 }

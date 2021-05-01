b2ff433c2125ed84e0510fb56cdccdd093b03afb
Attia Raed <raed.attia@polytechnicien.tn>
2021-05-01 17:31:51 +0100

    changed category controller messages

--- src/main/java/com/teamyostrik/efrip/controllers/CategoryController.java
+++ src/main/java/com/teamyostrik/efrip/controllers/CategoryController.java
@@ -25,38 +25,29 @@ public class CategoryController {
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
+
     @GetMapping
-    public List<Category> getAllCategories(){
+    public List<Category> getAllCategories() {
         return categoryService.getAllCategories();
     }
-    @GetMapping (path = "{categoryid}")
-    public Optional<Category> getCategory(@PathVariable ("categoryid") String id){
+
+    @GetMapping(path = "{categoryid}")
+    public Optional<Category> getCategory(@PathVariable("categoryid") String id) {
         return categoryService.getCategory(id);
     }
+
     @PostMapping
-    public ResponseEntity<HashMap<Object,Object>> addCategory(@RequestBody Category category){
-
-        Category categoryNameExists = categoryService.findCategoryByName(category.getName());
-        HashMap<Object,Object> model = new HashMap<>();
-        if(categoryNameExists != null){
-            model.put("success",1);
-            model.put("message","category "+category.getName()+" added successfully");
-            categoryService.addCategory(category);
-        }
-        else {
-            model.put("success",0);
-            model.put("message","Category already Exists !");
-        }
-        return ok(model);
-
-
+    public void addCategory(@RequestBody Category category) {
+        categoryService.addCategory(category);
     }
-    @DeleteMapping(path ="{categoryid}")
-    public void deleteCategory(@PathVariable ("categoryid") String id) {
+
+    @DeleteMapping(path = "{categoryid}")
+    public void deleteCategory(@PathVariable("categoryid") String id) {
         categoryService.deleteCategory(id);
     }
+
     @PostMapping(path = "{categoryid}")
-    public void updateCategory(@PathVariable ("categoryid") String id, Category category) {
+    public void updateCategory(@PathVariable("categoryid") String id, Category category) {
         categoryService.updateCategory(id, category);
     }
 

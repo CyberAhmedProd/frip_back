a9c8936cbc11517e5a51ba04fbeb39058d3c8442
Attia Raed <raed.attia@polytechnicien.tn>
2021-05-04 22:52:14 +0100

    just some cleaning

--- src/main/java/com/teamyostrik/efrip/FripApplication.java
+++ src/main/java/com/teamyostrik/efrip/FripApplication.java
@@ -2,8 +2,10 @@ package com.teamyostrik.efrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class FripApplication {
 
 	public static void main(String[] args) {

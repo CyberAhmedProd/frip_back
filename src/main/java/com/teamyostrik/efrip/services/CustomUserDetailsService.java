a1351f581b712ae1f5112db0e2315931fc8fd730
CyberAhmedProd <gharbiahmedpro@gmail.com>
2021-05-11 12:09:55 +0200

    fix regsiter add default photo profil and address

--- src/main/java/com/teamyostrik/efrip/services/CustomUserDetailsService.java
+++ src/main/java/com/teamyostrik/efrip/services/CustomUserDetailsService.java
@@ -17,10 +17,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.teamyostrik.efrip.models.Address;
+import com.teamyostrik.efrip.models.Photo;
+import com.teamyostrik.efrip.models.Profil;
 import com.teamyostrik.efrip.models.Role;
 import com.teamyostrik.efrip.models.Status;
 import com.teamyostrik.efrip.models.User;
 import com.teamyostrik.efrip.repositories.AddressRepository;
+import com.teamyostrik.efrip.repositories.PhotoRepository;
+import com.teamyostrik.efrip.repositories.ProfilRepository;
 import com.teamyostrik.efrip.repositories.RoleRepository;
 import com.teamyostrik.efrip.repositories.UserRepository;
 
@@ -39,6 +43,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 	private PasswordEncoder bCryptPasswordEncoder;
 	@Autowired
 	private AddressRepository addressRepository;
+	@Autowired
+	private ProfilRepository profilRepository;
+	@Autowired
+	private PhotoRepository photoRepository;
+
 	
 	public User findUserByEmail(String email) {
 	    return userRepository.findByEmail(email);
@@ -49,7 +58,22 @@ public class CustomUserDetailsService implements UserDetailsService {
 	    user.setStatus(Status.pending);
 	    Role userRole = roleRepository.findByRole(user.getRoles().iterator().next().getRole());
 	    user.setRoles(new HashSet<>(Arrays.asList(userRole)));
-	    userRepository.save(user);
+	    Profil profilData = new Profil();
+    	profilData.setFirstName("foulen");
+    	profilData.setLastName("BenFoulen");
+    	Photo img = new Photo();
+    	img.setTitle("image/png");
+    	//String file  = "aa";
+    	//img.setImage(new Binary(BsonBinarySubType.BINARY, file.getBytes())); 
+    	profilData.setAvatar(photoRepository.save(img));	
+    	Address address = new Address();
+    	address.setCity(" ");
+    	address.setCodePostal((short) 0000);
+    	address.setCountry(" ");
+    	address.setStreet(" ");
+    	profilData.setAddress(addressRepository.save(address));
+    	profilData.setUser(userRepository.save(user));
+    	profilRepository.save(profilData);
 	}
 	
 	@Override

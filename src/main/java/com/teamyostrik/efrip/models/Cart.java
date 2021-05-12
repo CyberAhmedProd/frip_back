1f2bebd6bdc135a644ebcc4182f2754dabe0e66d
Attia Raed <raed.attia@polytechnicien.tn>
2021-05-12 17:34:46 +0100

    added Cart

--- src/main/java/com/teamyostrik/efrip/models/Cart.java
+++ src/main/java/com/teamyostrik/efrip/models/Cart.java
@@ -1,6 +1,5 @@
 package com.teamyostrik.efrip.models;
 
-import org.graalvm.compiler.nodes.NodeView;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;

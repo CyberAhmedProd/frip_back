9bc9830325173ca382b8478909332719264ace83
Attia Raed <raed.attia@polytechnicien.tn>
2021-05-12 18:19:33 +0100

    added Cart

--- src/main/java/com/teamyostrik/efrip/repositories/CartRepository.java
+++ src/main/java/com/teamyostrik/efrip/repositories/CartRepository.java
@@ -12,5 +12,5 @@ import java.util.Optional;
 @Repository
 public interface CartRepository extends MongoRepository<Cart, String> {
     List<Cart> findAllByUser(User user);
-    Optional<Cart> findByProductAndUser_id(Product product, User user);
+    Optional<Cart> findByProductAndUser(Product product, User user);
 }
